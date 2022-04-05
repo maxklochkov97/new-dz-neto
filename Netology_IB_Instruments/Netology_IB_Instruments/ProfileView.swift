751f34d80dbb72d60f66f81471bb3b1ad3ce0e7d
Maksim <maximklochov@gmail.com>
2022-04-05 13:33:10 +0300

    ДЗ 1-2

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Максим Клочков on 31.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var cityOfBirthLabel: UILabel!
+    @IBOutlet weak var someUITextView: UITextView!
+}
