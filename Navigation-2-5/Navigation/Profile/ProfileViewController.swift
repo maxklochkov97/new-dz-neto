a030870260cb2e0e69dfdfe11067cd8506cacaef
Maksim <maximklochov@gmail.com>
2022-04-25 13:57:32 +0300

    ДЗ 2-5 - v4

--- Navigation-2-5/Navigation/Profile/ProfileViewController.swift
+++ Navigation-2-5/Navigation/Profile/ProfileViewController.swift
@@ -10,19 +10,8 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     // 2-5
-    var photoModel = Photo.allP()
-
-    private lazy var photoTableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(PhotoTableViewCell.self, forCellReuseIdentifier: PhotoTableViewCell.identifier)
-        return tableView
-    }()
+    var photoModel = Photo.allPhotos()
     // 2-5
-
-
     
     private let postModel: [Post] = Post.allPosts()
 
@@ -42,11 +31,16 @@ class ProfileViewController: UIViewController {
         setupView()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
     private func setupView() {
         self.view.backgroundColor = ColorSet.secondColor
+
         [self.tableView].forEach({ self.view.addSubview($0)})
 
-        
         NSLayoutConstraint.activate([
             self.tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
@@ -58,17 +52,35 @@ class ProfileViewController: UIViewController {
 
 // MARK: - UITableViewDataSource
 extension ProfileViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  postModel.count
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return  postModel.count
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        if indexPath.item == 0 {
+        if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: PhotoTableViewCell.identifier, for: indexPath) as! PhotoTableViewCell
             cell.configure(photos: photoModel)
-            cell.separatorInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+            cell.selectionStyle = .none
+            cell.delegate = self
+            cell.separatorInset = UIEdgeInsets(top: 0, left: cell.bounds.size.width, bottom: 0, right: 0)
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
@@ -84,17 +96,33 @@ extension ProfileViewController: UITableViewDataSource {
 extension ProfileViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.item == 0 {
-            return 100
-
+        if indexPath.section == 0 {
+            return 150
         } else {
             return UITableView.automaticDimension
         }
     }
-    
+
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        2
+    }
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = ProfileHeaderView()
-        header.backgroundColor = ColorSet.secondColor
-        return header
+        if section == 0 {
+            let header = ProfileHeaderView()
+            header.backgroundColor = ColorSet.secondColor
+            return header
+        } else {
+            return nil
+        }
+    }
+}
+
+// MARK: - ButtonDelegate
+
+extension ProfileViewController: CustomCellDelegate {
+    func buttonPressed() {
+        let newView = AllPhotoViewController()
+        navigationController?.pushViewController(newView, animated: true)
     }
 }
